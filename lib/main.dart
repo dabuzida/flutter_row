ad32f229935ef61e9edd4052781a4cdbadf2d7cc
dabuzida <dabuzida@gmail.com>
2022-05-25 14:27:44 +0900

    220525

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Row',
-      home: AAA(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Row'),
+          elevation: 0,
+          foregroundColor: Colors.cyanAccent,
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.greenAccent[100],
+        body: const AAA(),
+      ),
     );
   }
 }
@@ -21,80 +31,71 @@ class AAA extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Row'),
-        elevation: 0,
-        foregroundColor: Colors.cyanAccent,
-        backgroundColor: Colors.black,
-        centerTitle: true,
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.indigo, width: 2),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.indigo, width: 2),
-        ),
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(width: 50),
-            Expanded(
-              flex: 1,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.amber,
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(width: 50),
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.amber,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.blue,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.red,
+              constraints: BoxConstraints(
+                minWidth: 50,
+                maxWidth: 130,
               ),
             ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.blue,
-              ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.green,
             ),
-            Expanded(
-              flex: 5,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-                constraints: BoxConstraints(
-                  minWidth: 50,
-                  maxWidth: 130,
-                ),
-              ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.purple,
             ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.green,
-              ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.teal,
             ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.purple,
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.teal,
-              ),
-            ),
-            SizedBox(width: 50),
-          ],
-        ),
+          ),
+          SizedBox(width: 50),
+        ],
       ),
     );
   }
